c9fdea3760c6343d929d5c2ff33154ec065f1de8
Marina Murashev <marina@hoteltonight.com>
2018-01-07 17:29:05 -0500

    implement enum filter

--- ch10_collection_processing/enum_maker.exs
+++ ch10_collection_processing/enum_maker.exs
@@ -14,4 +14,27 @@ defmodule EnumMaker do
       false
     end
   end
+
+  def each(collection, fun) do
+    for item <- collection do
+      fun.(item)
+    end
+  end
+
+  def filter(collection, fun) do
+    filter_with_fun(collection, fun, [])
+  end
+
+
+  defp filter_with_fun([head | tail], fun, current) do
+    if fun.(head) do
+      filter_with_fun(tail, fun, current ++ [head])
+    else
+      filter_with_fun(tail, fun, current)
+    end
+  end
+
+  defp filter_with_fun([], fun, current) do
+    current
+  end
 end
