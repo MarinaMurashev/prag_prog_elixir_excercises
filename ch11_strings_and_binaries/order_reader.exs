e453f15c5f26699d874ea473c06d1ec79ddf5769
Marina Murashev <marina@hoteltonight.com>
2018-01-15 13:08:57 -0500

    cleaner version of order list reader

--- ch11_strings_and_binaries/order_reader.exs
+++ ch11_strings_and_binaries/order_reader.exs
@@ -5,14 +5,22 @@ defmodule OrderReader do
     File.open!(file)
       |> IO.stream(:line)
       |> Stream.drop(1)
-      |> Stream.map(&(String.trim_trailing(&1, "\n")))
-      |> Stream.map(&(String.split(&1, ",")))
-      |> Stream.map(&([id: String.to_integer(Enum.at(&1, 0)), ship_to: String.to_atom(String.trim_leading(Enum.at(&1, 1), ":")), net_amount: String.to_float(Enum.at(&1, 2))]))
+      |> Stream.map(&transform_line/1)
       |> Enum.to_list
       |> list_orders(tax_rates)
   end
 
   defp tax_rates do
     [NC: 0.075, TX: 0.08]
+  end
+
+  defp transform_line(line) do
+    list = String.trim_trailing(line, "\n") |> String.split(",")
+
+    id = Enum.at(list, 0) |> String.to_integer
+    ship_to = Enum.at(list, 1) |> String.trim_leading(":") |> String.to_atom
+    net_amount = Enum.at(list, 2) |> String.to_float
+
+    [id: id, ship_to: ship_to, net_amount: net_amount]
   end
 end
